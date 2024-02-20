146b2acdf65122105bccd07395420315dc1ec31b
anikdey095 <157403107+anikdey095@users.noreply.github.com>
2024-02-21 02:19:55 +0600

    Create codeforces 1760A-medium number.cpp

---
+++ codeforces 1760A-medium number.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+    int a, b, c, max = 0, min = 0;
+    cin >> a >> b >> c;
+    if (a > b && a > c)
+    {
+        max = a;
+    }
+    else if (b > a && b > c)
+    {
+        max = b;
+    }
+    else
+    {
+        max = c;
+    }
+    if (a < b && a < c)
+    {
+        min = a;
+    }
+    else if (b < c && b < c)
+    {
+        min = b;
+    }
+    else
+    {
+        min = c;
+    }
+    cout << (a+b+c)-(max + min)<<endl;
+    }
+}
