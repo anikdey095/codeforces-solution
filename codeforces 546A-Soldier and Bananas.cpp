129e7d8a67b6de77e4d4984b38014c8b4714711e
anikdey095 <157403107+anikdey095@users.noreply.github.com>
2024-02-21 03:56:06 +0600

    Create codeforces 546A-Soldier and Bananas.cpp

---
+++ codeforces 546A-Soldier and Bananas.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int k, n, w,sum=0;
+    cin >> k >> n >> w;
+    for (int i = 1; i <= w;i++)
+    {
+        sum +=(i*k);
+    }if((sum-n)<0){
+        cout << "0" << endl;
+    }
+    else{
+        cout << sum - n << endl;
+    }
+}
