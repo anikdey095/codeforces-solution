a7416e26937345bfe9261015674d0481895de2a9
anikdey095 <157403107+anikdey095@users.noreply.github.com>
2024-03-03 22:22:51 +0600

    Create 344A-Magnets.cpp

---
+++ 344A-Magnets.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, i, c = 0;
+    cin >> n;
+    int a[n];
+    i = 0;
+    for (i = 0; i < n; i++)
+        cin >> a[i];
+ 
+  
+ 
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] != a[i + 1])
+            c++;
+    }
+    cout << c << endl;
+    return 0;
+}
